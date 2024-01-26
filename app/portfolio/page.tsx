bb3d68e545a5e0df8438f7508c02a33889d61126
udilbar <ndilbar1618@gmail.com>
2024-01-26 14:44:05 +0500

    [update] get categories from database & render

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -14,12 +14,14 @@ import { createClient } from "@/utils/supabase/client";
 import { useEffect, useState } from "react";
 
 export default function Portfolio() {
-  const [tabs, setTabs] = useState<any[] | null>(null)
+  const [tabs, setTabs] = useState<Category[] | null>(null);
+  const [query, setQuery] = useState<string>("");
   const supabase = createClient();
+
   useEffect(() => {
     const getData = async () => {
-      const { data } = await supabase.from('categories').select('*');
-      setTabs(data);
+      const { data: categories } = await supabase.from('categories').select('*');
+      setTabs(categories);
     }
     getData()
   }, []);
@@ -29,11 +31,11 @@ export default function Portfolio() {
       <h2 className="tracking-[-1.04px] leading-[68px] uppercase font-semibold text-[52px] mb-[40px]">
         Our work
       </h2>
-      <Tabs defaultValue="all">
+      <Tabs defaultValue={query || "all"}>
         <TabsList className="mb-[40px] p-0 bg-transparent">
           <TabsTrigger value="all" className="border-b-[3px] py-3 px-5 rounded-none border-border/10 font-bold leading-[140%] tracking-[-0.18px] text-[18px] data-[state=active]:shadow-none data-[state=active]:text-primary data-[state=active]:border-primary">All models</TabsTrigger>
           {
-            tabs?.map((tab: any) => (
+            tabs?.map((tab: Category) => (
               <TabsTrigger key={tab.id} value={tab.name} className="border-b-[3px] py-3 px-5 rounded-none border-border/10 font-bold leading-[140%] tracking-[-0.18px] text-[18px] data-[state=active]:shadow-none data-[state=active]:text-primary data-[state=active]:border-primary">{tab.name}</TabsTrigger>
             ))
           }
@@ -41,7 +43,13 @@ export default function Portfolio() {
         <TabsContent value="all">
           <Projects />
         </TabsContent>
-        <TabsContent value="furniture">Furnitures.</TabsContent>
+        {
+          tabs?.map((tab: Category) => (
+            <TabsContent key={tab.id} value={tab.name}>
+              <Projects />
+            </TabsContent>
+          ))
+        }
       </Tabs>
 
       <Pagination>
@@ -67,4 +75,9 @@ export default function Portfolio() {
       </Pagination>
     </section>
   );
+}
+
+export interface Category {
+  id: string;
+  name: string;
 }
