1806d2d6a98752e7e450f76634abe95ee5ec47ed
udilbar <ndilbar1618@gmail.com>
2024-01-26 14:26:12 +0500

    [fix] next/image width&height error

--- components/TimelineItem.tsx
+++ components/TimelineItem.tsx
@@ -7,7 +7,7 @@ export default function TimelineItem({
   return (
     <div className="timeline-content relative max-w-[185px] w-full text-center shrink-0 grow-0 self-start relative py-2">
       <div className="timeline-icon absolute w-[50px] h-[50px] -translate-x-2/4 left-2/4 top-[calc(100%_+_90px)]">
-        <Image src={`/${icon}.png`} alt={icon} width={50} height={50} />
+        <Image className="w-[50px] h-[50px]" src={`/${icon}.png`} alt={icon} width={50} height={50} />
       </div>
       <div className="timieline-title text-[20px] font-medium leading-[28px] tracking-[-0.2px]">{text}</div>
     </div>
