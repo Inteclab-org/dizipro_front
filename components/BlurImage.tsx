4cb6ce94a29d9319c5f4579e7638c3e847d8d903
udilbar <ndilbar1618@gmail.com>
2024-01-26 15:07:42 +0500

    [fix] blur image styles

--- components/BlurImage.tsx
+++ components/BlurImage.tsx
@@ -8,25 +8,22 @@ export default function BlurImage(props: Project) {
   const [isLoading, setLoading] = useState(true);
 
   return (
-    <a href={props.id} className="group">
-      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-        <Image
-          loading="eager"
-          alt={props.name}
-          src={`${STORAGE_URL}${props.src}`}
-          width={238}
-          height={238}
-          objectFit="cover"
-          className={cn(
-            'w-[238px] h-[238px] group-hover:opacity-75 duration-700 ease-in-out',
-            isLoading
-              ? 'grayscale blur-2xl scale-110'
-              : 'grayscale-0 blur-0 scale-100'
-          )}
-          onLoadingComplete={() => setLoading(false)}
-        />
-      </div>
-      <h3 className="mt-4 text-sm text-gray-700">{props.name}</h3>
-    </a>
+    // <a href={props.id} className="group">
+      <Image
+        loading="eager"
+        alt={props.name}
+        src={`${STORAGE_URL}${props.src}`}
+        width={238}
+        height={238}
+        objectFit="cover"
+        className={cn(
+          'w-[238px] h-[238px] group-hover:opacity-75 duration-700 ease-in-out',
+          isLoading
+            ? 'grayscale blur-2xl scale-110'
+            : 'grayscale-0 blur-0 scale-100'
+        )}
+        onLoadingComplete={() => setLoading(false)}
+      />
+    // </a>
   );
 }
--- components/Project.tsx
+++ components/Project.tsx
@@ -18,7 +18,7 @@ export default function Project(props: Project) {
   return (
     <Dialog>
       <DialogTrigger>
-      <li className="bg-white flex flex-col py-6 pr-[25px] pl-[23px] border border-border/15 cursor-pointer hover:border-border/25 hover:shadow-[0px_12px_20px_0px_rgba(0,0,0,0.07)] hover:scale-110 transition-all duration-[0.3]">
+      <li className="bg-white flex flex-col py-6 pr-[25px] pl-[23px] border border-border/15 cursor-pointer hover:border-border/25 hover:shadow-[0px_12px_20px_0px_rgba(0,0,0,0.07)] hover:scale-110 transition-all duration-[0.3] overflow-hidden">
         {/* <Image
           className="w-[238px] h-[238px] object-cover"
           loading="eager"
