13b2e3887e701aac02da40c9986312f6945af64f
udilbar <ndilbar1618@gmail.com>
2024-09-19 11:15:37 +0500

    [add] responsiveness of footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,17 +14,27 @@ const Footer = memo(({
 }) => {
   return (
     <footer className="max-w-[1112px] w-full pt-[41px] pb-[12px] px-4 flex flex-col gap-[32px] leading-[22px]">
-        <div className="flex items-start justify-between gap-[40px]">
+        <div className="flex gap-[40px] flex-col items-start sm:items-center sm:flex-row md:justify-between md:items-start">
           <Link href={`/${locale}/`}>
             <Logo className="shrink-0" />
           </Link>
-          <div className="flex flex-col gap-[13px]">
-            <p className="font-semibold tracking-[-0.18px]">
-              {translation("footer.contact-us")}
-            </p>
-            <div className="flex gap-[4px] text-base font-medium text-foreground/65">
-              <Mail className="w-[22px] h-[22px] shrink-0" />
-              diziprocompany@gmail.com
+          <div className="flex justify-between flex-col gap-[32px] sm:ml-[100px] md:ml-0 md:gap-[93px] md:flex-row">
+            <div className="flex flex-col gap-[13px]">
+              <p className="font-semibold tracking-[-0.18px]">
+                {translation("footer.phone-number")}
+              </p>
+              <div className="text-[22px] font-bold">
+                +998(99)9875432
+              </div>
+            </div>
+            <div className="flex flex-col gap-[13px]">
+              <p className="font-semibold tracking-[-0.18px]">
+                {translation("footer.contact-us")}
+              </p>
+              <div className="flex gap-[4px] text-base font-medium text-foreground/65">
+                <Mail className="w-[22px] h-[22px] shrink-0" />
+                diziprocompany@gmail.com
+              </div>
             </div>
           </div>
         </div>
