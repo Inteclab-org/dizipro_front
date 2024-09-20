a63e0b93d31a737ba71a8c087bcf91c8da8cd0fa
udilbar <ndilbar1618@gmail.com>
2024-09-20 12:13:17 +0500

    [update] responsiveness of timeline

--- components/TimelineItem.tsx
+++ components/TimelineItem.tsx
@@ -10,13 +10,13 @@ export default function TimelineItem({
   return (
     <div className="timeline-content relative max-w-[185px] w-full text-center shrink-0 grow-0 self-start relative p-2">
       <div className="timeline-icon absolute w-[50px] h-[50px] -translate-x-2/4 left-2/4 top-[calc(100%_+_90px)]">
-        <Image className="hidden w-[50px] h-[50px] md:block" src={`/${icon}.png`} alt={icon} width={50} height={50} />
+        <Image className="w-[50px] h-[50px]" src={`/${icon}.png`} alt={icon} width={50} height={50} />
       </div>
       <motion.div
         className={
           cn(
-            "relative timieline-title text-[16px] font-medium leading-[28px] tracking-[-0.2px] md:text-[20px]",
-            index === 6 ? "whitespace-nowrap" : ""
+            "relative timieline-title text-[16px] font-medium tracking-[-0.16px] leading-[19px] whitespace-pre-line md:leading-[28px] md:text-[18px] md:tracking-[-0.2px]",
+            (index && index > 4) ? "whitespace-nowrap" : ""
           )
         }
         initial="hidden"
@@ -26,7 +26,7 @@ export default function TimelineItem({
         custom={index ? index : 0}
       >
         {text}
-        <div className="absolute border top-[-8px] left-[4px] w-[calc(100%-8px)] h-[calc(100%+16px)] md:left-[-14px] md:w-[calc(100%+28px)]"></div>
+        <div className="absolute border top-[-8px] w-full h-[calc(100%+16px)] md:left-[-14px] md:w-[calc(100%+28px)]"></div>
       </motion.div>
     </div>
   )
