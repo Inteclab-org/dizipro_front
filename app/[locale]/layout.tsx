5664e12672ef697b5600a66f66265ce04060d29c
udilbar <ndilbar1618@gmail.com>
2024-09-25 19:42:45 +0500

    [update] metadata

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,5 +1,4 @@
 import "../globals.css";
-import type { Viewport } from "next";
 import { GeistSans } from "geist/font/sans";
 import PartnerWithUs from "@/components/PartnerWithUs";
 import Footer from "@/components/Footer";
@@ -15,14 +14,14 @@ const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 
-export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "WE MAKE HIGH QUALITY 3D MODELS FOR YOU",
-  description: "Dizipro crafts top-notch 3D models online, powered by our skilled freelance team.",
-  other: {
-    "p:domain_verify": "a90a1f40513d7cb45fce1cf96e039edd"
-  }
-};
+// export const metadata = {
+//   metadataBase: new URL(defaultUrl),
+//   // title: "WE MAKE HIGH QUALITY 3D MODELS FOR YOU",
+//   // description: "Dizipro crafts top-notch 3D models online, powered by our skilled freelance team.",
+//   other: {
+//     "p:domain_verify": "a90a1f40513d7cb45fce1cf96e039edd"
+//   }
+// };
 
 export async function generateStaticParams() {
   return i18nConfig.locales.map((locale: Locale) => ({ locale: locale }));
@@ -39,12 +38,21 @@ export default async function RootLayout({ children, params }: Props) {
   const translation = await getTranslation(params.locale);
 
   return (
-    <html lang="en" className={GeistSans.className}>
+    <html lang={params.locale} className={GeistSans.className}>
       <Head>
+        <title>{translation("metadata.title")}</title>
+        <meta name="description" content={translation("metadata.description")} />
         <link
           href="https://fonts.cdnfonts.com/css/sf-pro-display"
           rel="stylesheet"
         />
+        <link rel="alternate" hrefLang="en" href={`${defaultUrl}/en`} />
+        <link rel="alternate" hrefLang="uz" href={`${defaultUrl}/uz`} />
+        <link rel="alternate" hrefLang="ru" href={`${defaultUrl}/ru`} />
+        <meta property="og:title" content={translation("metadata.title")} />
+        <meta property="og:description" content={translation("metadata.description")} />
+        <meta name="twitter:title" content={translation("metadata.title")} />
+        <meta name="twitter:description" content={translation("metadata.description")} />
       </Head>
       <body className="w-full min-h-screen flex flex-col items-center">
         <NextTopLoader />
