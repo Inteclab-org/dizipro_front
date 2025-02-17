d5a226b0d51a42ee58613d657f4774bb6ecb36a6
Dilbar Urinboeva <ndilbar1618@gmail.com>
2025-02-17 18:03:42 +0500

    [add] register page

--- app/[locale]/register/page.tsx
+++ app/[locale]/register/page.tsx
@@ -11,23 +11,22 @@ export default function Login({
   params: { locale: Locale };
 }) {
   
-  const signIn = async (formData: FormData) => {
+  const signUp = async (formData: FormData) => {
     "use server";
 
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
     const supabase = createClient();
 
-    const { error } = await supabase.auth.signInWithPassword({
+    const { error } = await supabase.auth.signUp({
       email,
       password,
     });
 
     if (error) {
-      console.log(error)
-      redirect(`/${params.locale}/login?message=Could not authenticate user`);
+      redirect(`/${params.locale}/register?message=Could not register user`);
     } else {
-      redirect(`/${params.locale}/portfolio/upload`);
+      redirect(`/${params.locale}/register?message=Confirm your email`);
     }
   };
 
@@ -35,7 +34,7 @@ export default function Login({
     <section className="flex flex-col max-w-[400px] w-full pt-[64px] pb-[153px]">
       <div className="flex-1 flex flex-col px-8 justify-center gap-2 space-y-8">
         <h2 className="tracking-[-1.04px] leading-[68px] text-center uppercase font-semibold text-[40px]">
-          Login
+          Register
         </h2>
 
         <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
@@ -59,11 +58,11 @@ export default function Login({
             required
           />
           <SubmitButton
-            formAction={signIn}
+            formAction={signUp}
             className="bg-gray-700 text-white rounded-md px-4 py-2 text-foreground mb-2"
-            pendingText="Signing In..."
+            pendingText="Signing Up..."
           >
-            Sign In
+            Sign Up
           </SubmitButton>
           
           {searchParams?.message && (
