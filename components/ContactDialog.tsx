bd59819c90e18ef8f863d2e6a1d1f8841a50b8fb
Dilbar Urinboeva <ndilbar1618@gmail.com>
2025-06-28 19:57:24 +0500

    [update] update phone number in ContactDialog and Footer components

--- components/ContactDialog.tsx
+++ components/ContactDialog.tsx
@@ -35,7 +35,7 @@ export default function ContactDialog({
         {
           locale === "uz" && <Link className="flex items-center gap-[8px] p-4 bg-muted" href="https://t.me/dizipro_order" target="_blank">
             <Phone className="w-[32px] h-[32px]" />
-            <Link className="font- text-[18px] tracking-[-0.56px] leading-[1.3]" href="tel:+998908178874" target="_blank">+998&nbsp;(90)&nbsp;817&nbsp;88&nbsp;74</Link>
+            <Link className="font- text-[18px] tracking-[-0.56px] leading-[1.3]" href="tel:+998910088874" target="_blank">+998&nbsp;(91)&nbsp;008&nbsp;88&nbsp;74</Link>
           </Link>
         }
         <div className="flex gap-2 justify-between">
